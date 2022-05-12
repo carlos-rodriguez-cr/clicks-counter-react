bb3d805687a7c6da75fbca555643767d59d48102
Carlos <carlosarieelr@gmail.com>
2022-05-12 14:56:26 -0300

    primer commit contador de clicks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import Boton from './components/Boton.jsx'
+import freeCodeCampLogo from './images/FreeCodeCamp-logo.png';
 
 function App() {
+
+  const manejarClick = () => {
+    console.log("Clic");
+  }
+
+  const reiniciarContador = () => {
+    console.log('Reiniciar');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='freecodecamp-logo-contenedor'>
+        <img
+          className='freecodecamp-logo'
+          src={freeCodeCampLogo}
+          alt='Logo de freeCodeCamp' /> 
+      </div>
+
+      <div className='contenedor-principal'>
+        <Boton 
+          texto='Clic'
+          esBotonDeClick={true}
+          manejarClick={manejarClick}/>
+        <Boton 
+        texto='Reiniciar'
+        esBotonDeClick={false}
+        manejarClick={reiniciarContador}/>
+      </div>
     </div>
   );
 }
